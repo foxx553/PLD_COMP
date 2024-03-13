946f13db8ccd1ecabf91246bc7823a9c0bf43054
swordfatih <accfldekur@gmail.com>
2024-03-13 08:28:05 +0100

    fix: gitignore

--- pld-comp/compiler/my_test.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-
-int main() {
-    int a = 5;
-    int b = rand();
-    putchar(a);
-    print();
-    return a + b;
-}
--- pld-comp/compiler/my_test2.c
+++
@@ -1,6 +0,0 @@
-int main() {
-    int a = 5;
-    int b = 0;
-    int c = 1;
-    putchar(a, b, 15);
-}
